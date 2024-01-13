13367cb9e9783a82d5fc46772ef989e3278554e1
Javier Quinteros <76599967+yeikyu@users.noreply.github.com>
2024-01-12 22:09:55 -0500

    Add files via upload

---
+++ menu_principal.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import tkinter as tk
+
+ventana = tk.Tk()
+
+ventana.configure(background='#afffcd')
+
+label1 = tk.Label(ventana, text="Este es mi label")
+
+label1.pack()
+
+ventana.mainloop()
